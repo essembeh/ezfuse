fbe03df1a729076f6ac3a5e53b8b9a16b13d9d9f
Sébastien MB <seb@essembeh.org>
2022-04-19 22:12:15 +0200

    ⬆️  Update dependencies

--- ezfuse/__init__.py
+++ ezfuse/__init__.py
@@ -2,10 +2,7 @@
 project metadata
 """
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
+import importlib.metadata as importlib_metadata
 
 __version__ = importlib_metadata.version(__name__)
 __title__ = "ezfuse"
